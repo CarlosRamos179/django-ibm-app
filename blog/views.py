22d8f98ef5d02adfc30cd79b638ec1c2d12f9194
Carlos Ramos <cramos@ndscognitivelabs.com>
2019-05-11 16:09:12 -0500

    add & edit buttons hide

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte = timezone.now()).order_by('publish_date')
@@ -13,12 +13,11 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    carlos = User.objects.get(username='carlos')
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = carlos
+            post.author = request.user #User.objects.get(username='carlos')
             post.published_date = timezone.now()
             post.publish()
             return redirect('post_detail', pk=post.pk)
@@ -27,13 +26,12 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
-    carlos = User.objects.get(username='carlos')
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = carlos
+            post.author = request.user #User.objects.get(username='carlos')
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
