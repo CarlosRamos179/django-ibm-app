53224af41efac7d022afe8a03fd704178e3c21c7
Carlos Ramos <cramos@ndscognitivelabs.com>
2019-05-28 14:46:55 -0500

    Componente que guarda, publica y elimina un post (borradores)

--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,8 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user #User.objects.get(username='carlos')
-            post.published_date = timezone.now()
-            post.publish()
+            post.author = request.user #User.objects.get(username='carlos'
+            post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -32,9 +31,23 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user #User.objects.get(username='carlos')
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(publish_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
