5fe8c2685254447b95f5b0162233f7baca822d4c
Carlos Ramos <cramos@ndscognitivelabs.com>
2019-05-29 15:44:07 -0500

    Autenticación del sitio

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 from blog import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/login/', views.login, name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
